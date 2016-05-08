4726280abfba3db974fea783f5466094380086f4
Rodrigo Vargas <rod.varga@gmail.com>
2016-05-08 19:39:52 -0400

    Added files via upload

--- titanic.R
+++ titanic.R
@@ -48,6 +48,24 @@ titanic_log_with_cabin$has_cabin_number
 #Write to csv
 write.csv(titanic_log_with_cabin, file = "titanic_clean.csv")
 
+#Some visualizations
+# Check out the structure of titanic
+str(titanic_log)
 
+# Use ggplot() for the first instruction
+ggplot(titanic_log, aes(x = factor(pclass), fill = factor(sex))) + geom_bar(position = "dodge")
+
+# Use ggplot() for the second instruction
+ggplot(titanic_log, aes(x = factor(pclass), fill = factor(sex))) + 
+  geom_bar(position = "dodge") + 
+  facet_grid(. ~ survived)
+
+# Position jitter (use below)
+posn.j <- position_jitter(0.5, 0)
+
+# Use ggplot() for the last instruction
+ggplot(titanic_log, aes(x = factor(pclass), y = age, col = factor(sex))) + 
+  geom_jitter(size = 3, alpha = 0.5, position = posn.j) + 
+  facet_grid(. ~ survived)
 
 
